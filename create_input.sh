1a3363852b90a86749353a7c714eb80dcd4312f5
konstantinos69 <sdi2000207@di.uoa.gr>
2023-05-23 14:16:23 +0300

    first bash script

---
+++ create_input.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+if [ $# != 2 ] || [ $2 -le 0 ]; then 
+    exit 0
+fi
+
+> $1
+
+CHARACTERS="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+a=$2
+while [ $a -ne 0 ];
+do
+    LENGTH1=$((RANDOM % 10 + 3))
+    STRING1=""
+    LENGTH2=$((RANDOM % 10 + 3))
+    STRING2=""
+    for ((i=0; i<LENGTH1; i++)); do
+        STRING1+=${CHARACTERS:RANDOM%${#CHARACTERS}:1}
+    done
+    for ((i=0; i<LENGTH2; i++)); do
+        STRING2+=${CHARACTERS:RANDOM%${#CHARACTERS}:1}
+    done
+    if [ $a -eq 1 ]; then
+        echo -n $STRING1 $STRING2>> $1
+    else 
+        echo $STRING1 $STRING2>> $1
+    fi
+    ((a -= 1))
+done
